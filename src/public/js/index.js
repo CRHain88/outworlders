afaaddf85ae1bb1ba33c945f386d0e2c3e66a0d7
Christian Hain <christian@crhain.com>
2017-04-15 10:48:26 -0400

    new: add eqjs to existing pages

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,7 @@
 import eqjs from 'eq.js';
+
+let contentContainers = document.querySelectorAll('.content').forEach((elm) => (
+    eqjs.definePts(elm, {
+        ['content--large']: 900,
+    })
+));
