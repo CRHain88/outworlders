4c2b667837d614d1f3349ef0745639fecf3b48ac
Christian Hain <christian@crhain.com>
2017-04-16 10:24:05 -0400

    new: add eqjs to index and contact page

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,7 +1,29 @@
 import eqjs from 'eq.js';
 
-let contentContainers = document.querySelectorAll('.content').forEach((elm) => (
+let contentContainers = document.querySelectorAll('[data-eq--content]').forEach((elm) => {
     eqjs.definePts(elm, {
         ['content--large']: 900,
-    })
-));
+    });
+
+    elm.addEventListener('eqResize', (event) => {
+        elm.querySelectorAll('[data-eq--card="contact"]').forEach((card) => {
+            let cardClasses = card.className;
+
+            if (event.detail === 'content--large') {
+                card.className = cardClasses.replace('card--dark', 'card--light');
+            } else {
+                card.className = cardClasses.replace('card--light', 'card--dark');
+            }
+        });
+
+        elm.querySelectorAll('[data-eq--card="officers"] .card__header, [data-eq--card="about-us"] .card__header').forEach((card) => {
+            let cardClasses = card.className;
+
+            if (event.detail === 'content--large') {
+                card.className = (cardClasses.indexOf('card__header--solid') === -1) ? cardClasses + ' card__header--solid' : cardClasses;
+            } else {
+                card.className = cardClasses.replace('card__header--solid', '').trim();
+            }
+        });
+    });
+});
